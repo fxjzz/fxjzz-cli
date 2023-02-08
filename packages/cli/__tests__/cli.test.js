6ed96ef81173ea88cc9c70efeac79eacb7f649b9
lzxzz <1042183935@qq.com>
2023-02-08 13:46:49 +0800

    feat: add jest test feature

--- packages/cli/__tests__/cli.test.js
+++ packages/cli/__tests__/cli.test.js
@@ -1,3 +1,9 @@
+import path from 'node:path'
+import { execa } from 'execa'
+
+const CLI = path.join(__dirname, '../bin/cli.js')
+const bin = () => () => execa(CLI)
+
 test('command error', () => {
-  expect(1 + 1).toBe(2)
+  bin()()
 })
